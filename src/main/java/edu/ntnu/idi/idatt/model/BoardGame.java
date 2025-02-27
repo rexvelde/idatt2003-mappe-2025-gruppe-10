498e32bc7410922446f9ddf20b4fa870ac24c1c0
rexvelde <harald.helland.velde@gmail.com>
2025-02-27 12:21:16 +0100

    Filled in attributes and methods in BoardGame

--- src/main/java/edu/ntnu/idi/idatt/model/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/model/BoardGame.java
@@ -1,4 +1,37 @@
 package edu.ntnu.idi.idatt.model;
 
+import java.util.List;
+
 public class BoardGame {
+    public Board board;
+    public Player currentPlayer;
+    public List<Player> players;
+    public Dice dice;
+
+    public BoardGame(Board board, Player currentPlayer, List<Player> players) {
+        this.board = board;
+        this.currentPlayer = currentPlayer;
+        this.players = players;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void createBoard() {
+        this.board = new Board();
+    }
+
+    public void createDice() {
+        dice = new Dice();
+    }
+
+    public void play() {
+        // TODO: play-method
+    }
+
+    public Player getWinner() {
+        // TODO: getWinner-method
+        return null;
+    }
 }

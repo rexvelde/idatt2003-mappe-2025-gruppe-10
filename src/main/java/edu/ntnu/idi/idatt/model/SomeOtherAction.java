56db8201a2bcfcbf62ef98ff35acf074d6fda10b
rexvelde <harald.helland.velde@gmail.com>
2025-02-27 13:36:51 +0100

    Filled action-template

--- src/main/java/edu/ntnu/idi/idatt/model/SomeOtherAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/SomeOtherAction.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idi.idatt.model;
 
 public class SomeOtherAction implements TileAction {
-  public void perform(Player player) {};
+  public void perform(Player player) {
+    // TODO: Perform some other action on player
+  };
 }
--- src/main/java/edu/ntnu/idi/idatt/stigespill/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/stigespill/LadderAction.java
@@ -4,5 +4,13 @@ import edu.ntnu.idi.idatt.model.Player;
 import edu.ntnu.idi.idatt.model.TileAction;
 
 public class LadderAction implements TileAction {
-  public void perform(Player player) {};
+  private int destination;
+
+  public LadderAction(int destination) {
+    this.destination = destination;
+  }
+
+  public void perform(Player player) {
+    // TODO: Perform some action on player
+  };
 }

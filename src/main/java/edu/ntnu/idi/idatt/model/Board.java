cd6ae4692d4bf349ea7f00881d994de4b4268049
rexvelde <harald.helland.velde@gmail.com>
2025-02-27 12:40:13 +0100

    Filled attributes and methods in Board and Tile

--- src/main/java/edu/ntnu/idi/idatt/model/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Board.java
@@ -1,4 +1,21 @@
 package edu.ntnu.idi.idatt.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+    public Map<Integer, Tile> tiles;
+
+    public Board() {
+        tiles = new HashMap<>();
+    }
+
+    public void addTile(Tile tile) {
+        int count = tiles.size();
+        tiles.put(count + 1, tile);
+    }
+
+    public Tile getTile(int tileId) {
+        return tiles.get(tileId);
+    }
 }
--- src/main/java/edu/ntnu/idi/idatt/model/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Tile.java
@@ -1,4 +1,23 @@
 package edu.ntnu.idi.idatt.model;
 
 public class Tile {
+    public Tile nextTile;
+    public int tileId;
+    public TileAction landAction;
+
+    public Tile(int tileId) {
+        this.tileId = tileId;
+    }
+
+    public void landPlayer(Player player) {
+        // TODO: fill landPlayer
+    }
+
+    public void leavePlayer(Player player) {
+        // TODO: fill leavePlayer
+    }
+
+    public void setNextTile(Tile nextTile) {
+        this.nextTile = nextTile;
+    }
 }

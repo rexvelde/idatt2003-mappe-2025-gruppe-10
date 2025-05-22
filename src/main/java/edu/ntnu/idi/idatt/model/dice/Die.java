6177accbb86d3d97f9ba88ce749fe3767ed2e626
rexvelde <harald.helland.velde@gmail.com>
2025-05-22 21:35:45 +0200

    DieTest, rather underwhelming with ONE function to test

--- src/main/java/edu/ntnu/idi/idatt/model/dice/Die.java
+++ src/main/java/edu/ntnu/idi/idatt/model/dice/Die.java
@@ -5,9 +5,8 @@ import java.util.Random;
 public class Die {
     private int lastRolledValue;
 
-    public int roll() {
+    public void roll() {
         lastRolledValue = new Random().nextInt(5) + 1;
-        return lastRolledValue;
     }
 
     public int getValue() {

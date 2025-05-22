df5203ddb30268dd8bb0e5e77be6beac64ef8e47
rexvelde <harald.helland.velde@gmail.com>
2025-05-22 13:16:42 +0200

    MediaPlayer now plays music

--- src/main/java/edu/ntnu/idi/idatt/controller/game/GooseGameController.java
+++ src/main/java/edu/ntnu/idi/idatt/controller/game/GooseGameController.java
@@ -1,23 +1,23 @@
-//package edu.ntnu.idi.idatt.controller.game;
-//
-//import edu.ntnu.idi.idatt.controller.menu.MainMenuController;
-//import edu.ntnu.idi.idatt.view.ViewManager;
-//import edu.ntnu.idi.idatt.view.game.GooseGameBoardView;
-//import edu.ntnu.idi.idatt.view.menu.MainMenuView;
-//
-//public class GooseGameController {
-//  private final GooseGameBoardView gooseGameBoardView;
-//
-//  public GooseGameController(GooseGameBoardView gooseGameBoardView) {
-//    this.gooseGameBoardView = gooseGameBoardView;
-//    initialize();
-//  }
-//
-//  private void initialize() {
-//    gooseGameBoardView.getMenuButton().setOnAction(e -> {
-//      MainMenuView mainMenuView = new MainMenuView();
-//      MainMenuController mainMenuController = new MainMenuController(mainMenuView);
-//      ViewManager.setRoot(mainMenuView);
-//    });
-//  }
-//}
+package edu.ntnu.idi.idatt.controller.game;
+
+import edu.ntnu.idi.idatt.controller.menu.MainMenuController;
+import edu.ntnu.idi.idatt.view.ViewManager;
+import edu.ntnu.idi.idatt.view.game.GooseGameBoardView;
+import edu.ntnu.idi.idatt.view.menu.MainMenuView;
+
+public class GooseGameController {
+  private final GooseGameBoardView gooseGameBoardView;
+
+  public GooseGameController(GooseGameBoardView gooseGameBoardView) {
+    this.gooseGameBoardView = gooseGameBoardView;
+    initialize();
+  }
+
+  private void initialize() {
+    gooseGameBoardView.getExitButton().setOnAction(e -> {
+      MainMenuView mainMenuView = new MainMenuView();
+      MainMenuController mainMenuController = new MainMenuController(mainMenuView);
+      ViewManager.setRoot(mainMenuView);
+    });
+  }
+}
